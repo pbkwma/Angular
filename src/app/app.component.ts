7d62f44ca383c165a5c1cfb00b4005a418589f68
pbkwma <Pbkarthi@rediffmail.com>
2026-02-14 14:28:07 +0530

    Add Angular dropdown component example

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { DropdownComponent } from './dropdown/dropdown.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [DropdownComponent],
+  template: '<app-dropdown></app-dropdown>'
+})
+export class AppComponent {}
